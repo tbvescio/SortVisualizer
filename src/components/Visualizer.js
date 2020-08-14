9f6e8a87d4e8cf6ea92b73031084a2957207db93
Tbvescio <tbvescio@gmail.com>
2020-08-14 11:01:18 -0300

    fixes

--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
+import "./Visualizer.css";
+
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
-import "./Visualizer.css";
+import Slider from "@material-ui/core/Slider";
+import Typography from "@material-ui/core/Typography";
 
 import Algorithms from "../algorithms/algorithms";
 
 const NUMBER_BARS = 100;
 const BAR_COLOR = "#ffbcbc";
-const SORT_SPEED = 1;
+// const SORT_SPEED = 1;
 
 class Visualizer extends Component {
   constructor(props) {
     super(props);
     this.isAnimating = false;
+    this.SORT_SPEED = 1;
   }
 
   state = {
@@ -24,6 +28,9 @@ class Visualizer extends Component {
   }
 
   resetArray() {
+    if (this.isAnimating) {
+      return;
+    }
     const array = [];
     for (let index = 0; index < NUMBER_BARS; index++) {
       array.push(this.randomIntFromInterval(5, 500));
@@ -31,11 +38,10 @@ class Visualizer extends Component {
     this.setState({ array });
   }
 
-  animationHandler(algorithm) {
+  async animationHandler(algorithm) {
     if (this.isAnimating === true) {
       return;
     }
-    this.isAnimating = true;
     let solution;
     switch (algorithm) {
       case "bubbleSort":
@@ -73,8 +79,13 @@ class Visualizer extends Component {
           arrayBars[solution[i].elements[0]].style.backgroundColor = "blue";
           arrayBars[solution[i].elements[1]].style.backgroundColor = "blue";
         }
+        if (i === solution.length - 1) {
+          this.isAnimating = false;
+        } else {
+          this.isAnimating = true;
+        }
         iStored = i;
-      }, i * SORT_SPEED);
+      }, i * this.SORT_SPEED);
     }
 
     //makes last bars red
@@ -87,6 +98,10 @@ class Visualizer extends Component {
 
   randomIntFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  sliderHandler = (value) => {
+    this.SORT_SPEED = value;
   };
 
   render() {
@@ -114,11 +129,43 @@ class Visualizer extends Component {
             disableElevation
             className="algo-btns"
           >
-            <Button onClick={() => this.animationHandler("bubbleSort")}>Bubble</Button>
-            <Button onClick={() => this.animationHandler("insertionSort")}>Insertion Sort</Button>
-            <Button onClick={() => this.animationHandler("selectionSort")}>Selection Sort</Button>
-            <Button onClick={() => this.animationHandler("shellSort")}>Shell Sort</Button>
+            <Button onClick={() => this.animationHandler("bubbleSort")}>
+              Bubble
+            </Button>
+            <Button onClick={() => this.animationHandler("insertionSort")}>
+              Insertion Sort
+            </Button>
+            <Button onClick={() => this.animationHandler("selectionSort")}>
+              Selection Sort
+            </Button>
+            <Button onClick={() => this.animationHandler("shellSort")}>
+              Shell Sort
+            </Button>
           </ButtonGroup>
+          <Typography
+            variant="button"
+            style={{
+              display: "inline-block",
+              color: "white",
+              marginLeft: "2em",
+            }}
+          >
+            Speed:
+          </Typography>
+          <Slider
+            defaultValue={1}
+            getAriaValueText={(e) => this.sliderHandler(e)}
+            valueLabelDisplay="auto"
+            step={1}
+            marks
+            min={0.25}
+            max={10}
+            style={{
+              width: "15em",
+              marginLeft: "2em",
+              verticalAlign: "middle",
+            }}
+          />
         </div>
       </div>
     );
